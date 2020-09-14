2aaa806cb4169930f5dc6a09013d7b9389c856dc
Sergiy <sergijmartynov@gmail.com>
2020-09-14 21:05:04 +0300

    third save

---
+++ tutorial.js
@@ -0,0 +1,21 @@
+const str = "test";
+const arr = [1, 2, 4];
+
+//console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+//console.log(logg.slic(6, 10));
+console.log(logg.substring(-26, 11));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+//console.log(parseInt(test));
+console.log(parseFloat(test));
